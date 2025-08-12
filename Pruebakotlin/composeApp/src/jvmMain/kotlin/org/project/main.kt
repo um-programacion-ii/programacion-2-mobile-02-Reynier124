2c273cc6c852c6b52ce0d94610023800fe7bf053
Reynier124 <reynier1306@gmail.com>
2025-08-12 18:27:19 -0300

    Prueba en KVM

---
+++ Pruebakotlin/composeApp/src/jvmMain/kotlin/org/project/main.kt
@@ -0,0 +1,13 @@
+package org.project
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Pruebakotlin",
+    ) {
+        App()
+    }
+}
